d110738c82525beea1307daa88edcf565ab88c8b
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-11-10 15:28:49 +0100

    jsoup methods and objects are used - initial reading of calendar

--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -1,12 +1,27 @@
 package com.example.demo.services;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 @org.springframework.stereotype.Service
 
 public class Service {
     //pliki_strony_kontroler/kalendarz.php?rok=" + rok + "&miesiac=" + miesiac + "&lang=" + lang;
     private static final String CALENDAR_ENDPOINT = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php";
-    public String getCalendar(int year, int month) {
+    public String getCalendar(int year, int month) throws IOException {
         //jsoup - biblioteka do pobierania danych z stron www
+
+        //pobieranie źródła strony www
+        Connection connect = Jsoup.connect(CALENDAR_ENDPOINT + "?rok=" + year + "&miesiac=" + month);
+        Document document = connect.get();
+
+        //wyciąganie informacji z dokumentu
+        Elements daysElements = document.select("a.active");
+        Elements eventNamesElements = document.select("div.InnerBox");
         return "year: " + year + " month: "+month;
     }
 }
