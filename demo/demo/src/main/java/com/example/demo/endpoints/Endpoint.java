c882bca758989f72a2622438293cb88630a5ad8f
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-11-10 14:26:47 +0100

    Service Class and Endpoint Class are created

---
+++ demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -0,0 +1,6 @@
+package com.example.demo.endpoints;
+
+public class Endpoint {
+
+
+}
---
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -0,0 +1,7 @@
+package com.example.demo.services;
+
+@org.springframework.stereotype.Service
+
+public class Service {
+
+}
