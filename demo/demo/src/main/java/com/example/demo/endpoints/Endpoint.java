4513e92d0c7bd0a6b30607098ab71d05b1d73e89
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-11-10 14:44:42 +0100

    test service for checking if everything works

--- demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
+++ demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -2,17 +2,20 @@ package com.example.demo.endpoints;
 
 import com.example.demo.services.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class Endpoint {
     private Service service;
-    
+
     @Autowired
     public Endpoint(Service service) {
         this.service = service;
     }
 
+    @GetMapping("/calendar/{year}/{month}")
+    public String getResult(@PathVariable int year, @PathVariable int month) {
+        return service.getCalendar(year,month);
+    }
 }
--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -3,5 +3,7 @@ package com.example.demo.services;
 @org.springframework.stereotype.Service
 
 public class Service {
-
+    public String getCalendar(int year, int month) {
+        return "year: " + year + " month: "+month;
+    }
 }
