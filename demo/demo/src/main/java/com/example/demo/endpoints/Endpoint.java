89dd62e069a14d74133fb38992fb22cf0445e9da
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-11-10 14:29:28 +0100

    Constructor in Endpoint Class is done

--- demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
+++ demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -1,6 +1,18 @@
 package com.example.demo.endpoints;
 
-public class Endpoint {
+import com.example.demo.services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
+public class Endpoint {
+    private Service service;
+    
+    @Autowired
+    public Endpoint(Service service) {
+        this.service = service;
+    }
 
 }
