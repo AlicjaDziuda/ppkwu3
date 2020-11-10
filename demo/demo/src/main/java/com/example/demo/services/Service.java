676630e3da4fff57b4c1d1cc3934c2d9fb1b83e9
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-11-10 14:59:58 +0100

    declared url for calendar to sending requests

--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -3,7 +3,10 @@ package com.example.demo.services;
 @org.springframework.stereotype.Service
 
 public class Service {
+    //pliki_strony_kontroler/kalendarz.php?rok=" + rok + "&miesiac=" + miesiac + "&lang=" + lang;
+    private static final String CALENDAR_ENDPOINT = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php";
     public String getCalendar(int year, int month) {
+        //jsoup - biblioteka do pobierania danych z stron www
         return "year: " + year + " month: "+month;
     }
 }
